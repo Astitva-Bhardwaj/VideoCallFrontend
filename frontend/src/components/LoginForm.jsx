8b0c2b600b57ff56b7a579f3baf50e8db4bb59e3
Aman5Techie <vermaji9585@gmail.com>
2024-04-02 21:21:00 +0530

    Added css

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,50 +1,127 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import './LoginForm.css';
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+import "./css/LoginFormcss.jsx";
+import LoginContainer from "./css/LoginFormcss.jsx";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import toastOption from "../toast_setting.js";
+import Logo from "../assets/logo.png"
+const LoginForm = () => {
+  const navigate = useNavigate();
+  const [istrue, settrue] = useState(false);
+  const [data, setdata] = useState({
+    username: "",
+    password: "",
+  });
 
-const loginForm = () => {
-    const navigate = useNavigate();
-    const [formData, setFormData] = useState({
-        username: '',
-        password: ''
-    });
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+  function getuserdata(event) {
+    setdata({ ...data, [event.target.name]: event.target.value });
+  }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post('http://localhost:8081/user/login', new URLSearchParams({
-                username: formData.username,
-                password: formData.password
-            }));
-            console.log('User logged in successfully!', response.data);
-           navigate('/meeting');
-            // Redirect or show a success message as needed
-        } catch (error) {
-            console.error('Login failed:', error);
-        }
-    };
+  async function submitform(event) {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:8081/user/login", new URLSearchParams({
+        username: data.username,
+        password: data.password,
+      }));
+      console.log("User logged in successfully!", response.data);
+      navigate("/meeting");
+    } catch (error) {
+      toast.error("Password is Incorrect", toastOption);
+    }
+  }
 
-    return (
-        <div className="container">
-            <h1>User Login</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label>Username:</label>
-                    <input type="text" name="username" value={formData.username} onChange={handleChange} />
-                </div>
-                <div className="form-group">
-                    <label>Password:</label>
-                    <input type="password" name="password" value={formData.password} onChange={handleChange} />
-                </div>
-                <input type="submit" value="Login" />
-            </form>
-        </div>
-    );
+  return (
+    <>
+      <LoginContainer>
+        <form
+          onSubmit={(event) => {
+            submitform(event);
+          }}
+        >
+          <div className="brand">
+            <img src={Logo} alt="Logo" />/
+           <div className="heading"> <h1>VisionConnect</h1></div>
+          </div>
+          
+
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={(e) => {
+              getuserdata(e);
+            }}
+          ></input>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={(e) => {
+              getuserdata(e);
+              if (e.target.value !== "") {
+                settrue(true);
+              } else {
+                settrue(false);
+              }
+            }}
+          ></input>
+
+          {istrue ? <button type="submit">submit</button> : ""}
+          <span>
+            Donot Have an Account ? <Link to="/register">Register</Link>
+          </span>
+        </form>
+      </LoginContainer>
+      <ToastContainer />
+    </>
+  );
 };
 
-export default loginForm;
+// const LoginForm = () => {
+//     const navigate = useNavigate();
+//     const [formData, setFormData] = useState({
+//         username: '',
+//         password: ''
+//     });
+//     const handleChange = (e) => {
+//         const { name, value } = e.target;
+//         setFormData({ ...formData, [name]: value });
+//     };
+
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+//         try {
+//             const response = await axios.post('http://localhost:8081/user/login', new URLSearchParams({
+//                 username: formData.username,
+//                 password: formData.password
+//             }));
+//             console.log('User logged in successfully!', response.data);
+//            navigate('/meeting');
+//             // Redirect or show a success message as needed
+//         } catch (error) {
+//             console.error('Login failed:', error);
+//         }
+//     };
+
+//     return (
+//         <div className="container">
+//             <h1>User Login</h1>
+//             <form onSubmit={handleSubmit}>
+//                 <div className="form-group">
+//                     <label>Username:</label>
+//                     <input type="text" name="username" value={formData.username} onChange={handleChange} />
+//                 </div>
+//                 <div className="form-group">
+//                     <label>Password:</label>
+//                     <input type="password" name="password" value={formData.password} onChange={handleChange} />
+//                 </div>
+//                 <input type="submit" value="Login" />
+//             </form>
+//         </div>
+//     );
+// };
+
+export default LoginForm;
--- frontend/src/components/MeetingForm.jsx
+++ frontend/src/components/MeetingForm.jsx
@@ -1,63 +1,114 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import MeetingFormrum from "./css/MeetingFormcss";
+
 const MeetingForm = () => {
-    const navigate = useNavigate();
-    const [callId, setCallId] = useState('');
-    const [joinCallId, setJoinCallId] = useState('');
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post('http://localhost:8081/user/video/create', { callId });
-            console.log('Video call created successfully!', response.data);
-            // Redirect or show a success message as needed
-            navigate('/meeting_button');
-        } catch (error) {
-            console.error('Error creating video call:', error);
-        }
-    };
-
-    const handleJoinCallSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            navigate('/camera');
-            // Send a POST request to join the video call
-            const response = await axios.post('http://localhost:8081/user/video/join/call', { joinCallId });
-            console.log('Joined video call successfully!', response.data);
-            // Redirect or show a success message as needed
-        } catch (error) {
-            console.error('Error joining video call:', error);
-        }
-    };
-
-    const handleChange = (e) => {
-        setCallId(e.target.value);
-    };
-    const handleJoinCallChange = (e) => {
-        setJoinCallId(e.target.value);
-    };
-
-
-    return (
-        <div className="container">
-            <h1>Create Video Call</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="createCallId">Call ID:</label>
-                    <input type="text" id="createCallId" name="callId" value={callId} onChange={handleChange} required />
-                </div>
-                <input type="submit" value="Create Call" />
-            </form>
-            <h1>Join Video Call</h1>
-            <form onSubmit={handleJoinCallSubmit}>
-                <div className="form-group">
-                    <label htmlFor="joinCallId">Enter Call ID:</label>
-                    <input type="text" id="joinCallId" name="joinCallId" value={joinCallId} onChange={handleJoinCallChange} required />
-                </div>
-                <input type="submit" value="Join Call" />
-            </form>
+  return (
+    <MeetingFormrum>
+      <div className="container">
+        <div className="create">
+          <h1>Create Video Call</h1>
+          <form onSubmit={() => {}}>
+            <div className="form-group">
+              <label className="lable" htmlFor="createCallId">
+                Create Call ID:
+              </label>
+              <input
+                type="text"
+                id="createCallId"
+                name="callId"
+                required
+                placeholder="Enter ID"
+              />
+              <div className="buttons-container">
+                <button>Create Call</button>
+                <button>Join Call</button>
+              </div>
+              <input type="submit" value="Create Call" />
+            </div>
+          </form>
         </div>
-    );
+        <div className="join">
+          <h1>Join Video Call</h1>
+          <form onSubmit={() => {}}>
+            <div className="form-group">
+              <label className="lable" htmlFor="joinCallId">
+                Enter Call ID:
+              </label>
+
+              <input
+                type="text"
+                id="joinCallId"
+                name="joinCallId"
+                required
+                placeholder="Enter ID"
+              />
+              <input type="submit" value="Join Call" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </MeetingFormrum>
+  );
 };
+
+// const MeetingForm = () => {
+//     const navigate = useNavigate();
+//     const [callId, setCallId] = useState('');
+//     const [joinCallId, setJoinCallId] = useState('');
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+//         try {
+//             const response = await axios.post('http://localhost:8081/user/video/create', { callId });
+//             console.log('Video call created successfully!', response.data);
+//             // Redirect or show a success message as needed
+//             navigate('/meeting_button');
+//         } catch (error) {
+//             console.error('Error creating video call:', error);
+//         }
+//     };
+
+//     const handleJoinCallSubmit = async (e) => {
+//         e.preventDefault();
+//         try {
+//             navigate('/camera');
+//             // Send a POST request to join the video call
+//             const response = await axios.post('http://localhost:8081/user/video/join/call', { joinCallId });
+//             console.log('Joined video call successfully!', response.data);
+//             // Redirect or show a success message as needed
+//         } catch (error) {
+//             console.error('Error joining video call:', error);
+//         }
+//     };
+
+//     const handleChange = (e) => {
+//         setCallId(e.target.value);
+//     };
+//     const handleJoinCallChange = (e) => {
+//         setJoinCallId(e.target.value);
+//     };
+
+//     return (
+// <div className="container">
+//     <h1>Create Video Call</h1>
+//     <form onSubmit={handleSubmit}>
+//         <div className="form-group">
+//             <label htmlFor="createCallId">Call ID:</label>
+//             <input type="text" id="createCallId" name="callId" value={callId} onChange={handleChange} required />
+//         </div>
+//         <input type="submit" value="Create Call" />
+//     </form>
+//     <h1>Join Video Call</h1>
+//     <form onSubmit={handleJoinCallSubmit}>
+//         <div className="form-group">
+//             <label htmlFor="joinCallId">Enter Call ID:</label>
+//             <input type="text" id="joinCallId" name="joinCallId" value={joinCallId} onChange={handleJoinCallChange} required />
+//         </div>
+//         <input type="submit" value="Join Call" />
+//     </form>
+// </div>
+//     );
+// };
 
 export default MeetingForm;
